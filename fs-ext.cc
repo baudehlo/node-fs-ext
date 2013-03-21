f6a5b80d6431776b485e9b57fe9ae27c8108cf60
David Brown <dave@scri.pt>
2013-03-21 11:34:32 -0700

    Compilation fixes for node v0.10.0.

--- fs-ext.cc
+++ fs-ext.cc
@@ -79,7 +79,7 @@ enum
   FS_OP_STATVFS
 };
 
-static void EIO_After(uv_work_t *req) {
+static void EIO_After(uv_work_t *req, int request) {
   HandleScope scope;
 
   store_data_t *store_data = static_cast<store_data_t *>(req->data);
